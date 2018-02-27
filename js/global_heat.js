196e947b76970480108cb8ec53db50399460a802
Student User <studentuser@D-173-250-236-45.dhcp4.washington.edu>
2018-02-26 16:50:03 -0800

    deleted the unused sections

--- js/global_heat.js
+++ js/global_heat.js
@@ -1,5 +1,4 @@
 $(function() {
-    //var patt = new RegExp("All");
     var dataset; //the full dataset
 
     var margin = { top: 20, right: 20, bottom: 40, left: 40 },
@@ -16,10 +15,6 @@ $(function() {
     // setup y
     var yValue = function (d) { return d["Lowess Smoothing"]; }, // data -> value
         yMap = function (d) { return yScale(yValue(d)); }; // data -> display
-
-    // // setup fill color
-    // var cValue = function (d) { return d["Employment Type"]; },
-    //     color = d3.scaleOrdinal(d3.schemeCategory10);
 
     var svg = d3.select("#vis").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -81,44 +76,9 @@ $(function() {
             .attr("cx", xMap)
             .attr("cy", yMap)
             .style("fill", "black")
-            // .style("fill", function (d) { return color(cValue(d)); })
-            // .on("mouseover", function (d) {
-            //     tooltip.transition()
-            //         .duration(200)
-            //         .style("opacity", .9);
-            //     tooltip.html(d["Job Class Title"]  + "<br/> Quarter 1 "+ 
-            //         d["Q1 Payments"] + "<br/> Quarter 2 " + d["Q2 Payments"] +
-            //         "<br/> Quarter 3 " + d["Q3 Payments"] +
-            //         "<br/> Quarter 4 " + d["Q4 Payments"] +  
-            //         "<br/> Average Health Cost " + d["Average Health Cost"] + 
-            //         "<br/> Average Dental Cost " + d["Average Dental Cost"] + 
-            //         "<br/> Average Basic Life " + d["Average Basic Life"] +
-            //         "<br/> (" + xValue(d) + ", " + (yValue(d) * 1000) + ")")
-            //         .style("left", (d3.event.pageX + 5) + "px")
-            //         .style("top", (d3.event.pageY - 28) + "px");
-            // })
-            // .on("mouseout", function (d) {
-            //     tooltip.transition()
-            //         .duration(500)
-            //         .style("opacity", 0);
-            // });
             dots
                 .attr("cx", xMap)
                 .attr("cy", yMap)
-
-/*         // draw legend
-        var legend = svg.selectAll(".legend")
-            .data(color.domain())
-            .enter().append("g")
-            .attr("class", "legend")
-            .attr("transform", function (d, i) { return "translate(0" + "," + i * 25 + ")"; }); */
-
-/*         // draw legend colored rectangles
-        legend.append("rect")
-            .attr("x", width - 10)
-            .attr("width", 18)
-            .attr("height", 18)
-            .style("fill", color); */
 
         // draw legend text
         legend.append("text")
@@ -127,27 +87,5 @@ $(function() {
             .attr("dy", ".35em")
             .style("text-anchor", "end")
             .text(function (d) { return d; })
-        
-        // add the tooltip area to the webpage
-        // var tooltip = d3.select("body").append("div")
-        //     .attr("class", "tooltip")
-        //     .style("opacity", 0);
-        
-        // document.querySelector('#selectForm').onchange = function() {
-        //     filterType(this.value)
-        // }
     }
-
-    // function filterType(type) {
-    //     //add code to filter to mytype and rerender vis here
-    //     var res = patt.test(type);
-    //     if(res){
-    //         drawVis(dataset);
-    //     } else{
-    //         var ndata = dataset.filter(function(d) {
-    //             return d['Department Title'] == type;
-    //         });
-    //         drawVis(ndata);
-    //     }
-    // }
 });
