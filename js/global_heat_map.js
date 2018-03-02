3755297206b4f8c4f93cd2a4c405def325ed6695
tiwans <tiwans@uw.edu>
2018-03-01 20:14:46 -0800

    map in progress

--- js/global_heat_map.js
+++ js/global_heat_map.js
@@ -1,11 +1,46 @@
-function drawGlobalHeatMap() {
-    d3.csv("data/global_heat_index_data.csv", function (error, data) {
+$(function() {
+    var width = 960,
+    height = 500;
+
+var projection = d3.geoAlbersUsa();
+
+var path = d3.geoPath()
+	.projection(projection);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.queue()
+    .defer(d3_request.json, "states.json")
+    .defer(d3_request.json, "us-state-centroids.json")
+    .await(ready);
+
+var us,centroid;
+
+function ready(error, u, c) {
+    states = u
+    centroid = c
+ 
+   // draw the states
+   svg.append("path")
+       .attr("class", "states")
+       .datum(topojson.feature(states, states.objects.usStates))
+       .attr("d", path);
+
+       d3.csv("data/global_heat_index_data.csv", function (error, data) {
         if (error) return console.warn(error);
             data.forEach(function (d) {
-                // lol do this
+                d.Latitude = +d.Latitude;
+                d.Longitude = +d.Longitude;
+                d.year = +d["1976-1977"];
         });
         dataset = data;
 
         drawVis(dataset);
     });
+    
+
 }
+    
+});
