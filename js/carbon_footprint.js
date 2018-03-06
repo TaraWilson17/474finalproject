e32c6f3913a97c3ce309c398a58b27bb36e7d9cc
zachh20 <zachh20@msn.com>
2018-03-06 15:52:28 -0800

    working on sliders

--- js/carbon_footprint.js
+++ js/carbon_footprint.js
@@ -10,6 +10,26 @@ $(function() {
     var yScale = d3.scaleLinear().range([height, 0]);
 
     var carFootPrintYearly = 10484;
+    var carMilesDriven = 11398;
+    var emissionsFromHome = 5455;
+    var emissionsFromGarbage = 692;
+    var yearlyEmissions = [carFootPrintYearly, emissionsFromHome, emissionsFromGarbage]
+    var labels = ['Car', 'Home Electricity Usage', 'Emissions from Garbage']
 
+    var svg = d3.select("#individual_footprint").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-}
+    $('#milseInput').on('change', function() {
+        console.log(this.value)
+        $('.slider_label').html(this.value);
+    })
+  
+
+    function drawBarGraph( ) {
+        svg.append('g').call(slider)
+            
+    }
+})
