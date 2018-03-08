f42a0cd46a143f41eace4299d8b9c9b7ba842717
tiwans <tiwans@uw.edu>
2018-03-08 14:01:50 -0800

    changed y axis scale

--- js/carbon_footprint.js
+++ js/carbon_footprint.js
@@ -71,7 +71,7 @@ $(function() {
     function drawBarGraph() {
         var data = [carFootPrintYearly, thisPersonsCar * 52 * (carFootPrintYearly/carMilesDriven), emissionsFromHome, emissionsFromHome * yourElectricity/100,  emissionsFromGarbage,  emissionsFromGarbage * yourGarbage/100]
        
-        var testLabels = [ 'Average Car', ' Your Car', 'Average electricity', 'Your Electricity', 'Average Garbage', 'Your Garbage']
+        var testLabels = [ 'Average Car', ' Your Car', 'Average Electricity', 'Your Electricity', 'Average Garbage', 'Your Garbage']
             xScale.domain(testLabels)
             yScale.domain([0, 12000])
 
