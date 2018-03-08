c74fa58f8a9090244e4b58693b83561ee623073e
tiwans <tiwans@uw.edu>
2018-03-08 13:42:08 -0800

    changed the format to match the other graphs

--- js/carbon_footprint.js
+++ js/carbon_footprint.js
@@ -1,9 +1,9 @@
 $(function() {
     var dataset; //the full dataset
 
-    var margin = { top: 20, right: 20, bottom: 40, left: 40 },
-    width = 500 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    var margin = { top: 20, right: 20, bottom: 40, left: 60 },
+        width = +700 - margin.left - margin.right,
+        height = +500 - margin.top - margin.bottom
 
     var xScale = d3.scaleBand()
           .range([0, width])
@@ -71,34 +71,34 @@ $(function() {
     function drawBarGraph() {
         var data = [carFootPrintYearly, thisPersonsCar * 52 * (carFootPrintYearly/carMilesDriven), emissionsFromHome, emissionsFromHome * yourElectricity/100,  emissionsFromGarbage,  emissionsFromGarbage * yourGarbage/100]
        
-    var testLabels = [ 'Average Car', ' Your Car', 'Average electricity', 'Your electricity', 'Average garbage', 'Your Garbage']
-        xScale.domain(testLabels)
-        yScale.domain([0, 12000])
+        var testLabels = [ 'Average Car', ' Your Car', 'Average electricity', 'Your Electricity', 'Average Garbage', 'Your Garbage']
+            xScale.domain(testLabels)
+            yScale.domain([0, 12000])
 
-        svg.selectAll('.bar').remove()
+            svg.selectAll('.bar').remove()
 
-    svg.selectAll(".bar")
-      .data(data)
-        .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d, i) {return xScale(testLabels[i]) })
-      .attr("width", 60)
-      .attr("y", function(d, i) { return yScale(data[i]); })
-      .attr("height", function(d, i) { return height - yScale(data[i]) })
-      .style('fill', function(d, i) {return colors(i)});
+        svg.selectAll(".bar")
+        .data(data)
+            .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d, i) {return xScale(testLabels[i]) })
+        .attr("width", 60)
+        .attr("y", function(d, i) { return yScale(data[i]); })
+        .attr("height", function(d, i) { return height - yScale(data[i]) })
+        .style('fill', function(d, i) {return colors(i)});
 
-  // add the x Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(xScale));
+        // add the x Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(xScale));
 
-  // add the y Axis
-  svg.append("g")
-      .call(d3.axisLeft(yScale));
+        // add the y Axis
+        svg.append("g")
+            .call(d3.axisLeft(yScale));
 
-    svg.append("text")
+        svg.append("text")
             .attr("transform", "rotate(-90)")
-            .attr("y", 0 - margin.left)
+            .attr("y", -60)
             .attr("x", 0 - (height / 2))
             .attr("dy", "1em")
             .style("text-anchor", "middle")
@@ -108,13 +108,10 @@ $(function() {
         // Adds title to the visual
         svg.append("text")
             .attr("x", (width / 2))             
-            .attr("y", 380)
+            .attr("y", 480)
+            .style("text-anchor", "middle")
             .attr("font-weight", "bold")
-            .attr("text-anchor", "middle")  
-            .style("font-size", "12px") 
             .text("Emission Source");
-
-
     }
 
     drawBarGraph()
